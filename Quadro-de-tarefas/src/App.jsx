ceef73cabb16985ac491249a90bbc7ec9f1c8f19
eunaotenhocriatividade <andreicruz1203@gmail.com>
2025-03-17 17:51:10 -0300

    comecei o projeto

---
+++ Quadro-de-tarefas/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Colunas from './colunas/colunas'
+
+function App() {
+
+  return (
+    <>
+     <h1>Quadro de tarefas com react</h1>
+     <main>
+      <Colunas />
+     </main>
+    </>
+  )
+}
+
+export default App
---
+++ Quadro-de-tarefas/src/colunas/colunas.jsx
@@ -0,0 +1,19 @@
+import Coluna from '../coluna/coluna'
+import './colunas.css'
+
+function Colunas() {
+   return (
+   <>
+    <section className='colunas'>
+        <Coluna titulo="A fazer"/>
+        <Coluna titulo="Em progresso"/>
+        <section coluna="coluna">
+        <section className="card-coluna">
+            <section className="card"></section>
+        </section>
+        </section>
+    </section>
+    </>
+)
+}
+export default Colunas
