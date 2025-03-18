38e20c2f10363c0bfb9daff236a910267f56652f
eunaotenhocriatividade <andreicruz1203@gmail.com>
2025-03-18 08:33:03 -0300

    "finalizei a parte da interação

--- Quadro-de-tarefas/src/cards/cards.jsx
+++ Quadro-de-tarefas/src/cards/cards.jsx
@@ -2,31 +2,64 @@ import './cards.css'
 
 function Cards() {
     const colunas = document.querySelectorAll(".card-colunas");
-    const cards = document.querySelectorAll(".card");
-
     let cardpego;
 
     const dragStart = (event) =>
     {
-        cardpego = event.target
-       event
+        cardpego = event.target;
+       event.dataTransfer.effectAllowed = "move";
     };
 
     const dragOver = (event)=>{
-        event.preventDefault()
+        event.preventDefault();
     };
 
-    
+    const dragEnter = ({target}) => {
+        if (target.classList.contains("card-colunas")) {
+            target.classList.add("foco-coluna")
+        }
+    }
 
-    colunas.forEach((coluna)=>{
-        coluna.addEventListener('dragover', dragOver)}
-    )
-    
-    cards.forEach((card)=>{
+    const dragLeave = ({target}) => {
+        target.classList.remove("foco-coluna")
+    }
+
+    const drop = ({target}) => {
+        if(!target.classList.contains('card-colunas')) return
+
+        target.append(cardpego)
+        target.classList.remove('foco-coluna')
+    }
+
+    const criarCard = ({target}) => {
+
+        if(!target.classList.contains('card-colunas')) return
+
+        const card = document.createElement('section');
+
+        card.className = "card"
+        card.draggable = "true"
+        card.contentEditable = "true"
+
+        card.addEventListener('focusout', ()=>{ 
+            card.contentEditable = "false"
+            if(!card.textContent) { card.remove()}
+        })
+        
         card.addEventListener('dragstart', dragStart)
+
+        target.append(card)
+        card.focus()
+    }    
+
+    colunas.forEach((coluna) =>{
+        coluna.addEventListener('dragover' , dragOver);
+        coluna.addEventListener("dragenter", dragEnter);
+        coluna.addEventListener('dragleave', dragLeave);
+        coluna.addEventListener('drop', drop);
+        coluna.addEventListener('dblclick', criarCard)
     })
-
-
+    
     return (
         <>
         <section className="card-colunas">
