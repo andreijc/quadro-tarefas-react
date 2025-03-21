c6cc536c52fe26b9303c0cd641e8f790b75eef97
eunaotenhocriatividade <andreicruz1203@gmail.com>
2025-03-21 08:39:51 -0300

    Update cards.jsx

--- Quadro-de-tarefas/src/cards/cards.jsx
+++ Quadro-de-tarefas/src/cards/cards.jsx
@@ -1,72 +1,71 @@
 import './cards.css'
 
 function Cards() {
-    const colunas = document.querySelectorAll(".card-colunas");
-    const cards = document.querySelectorAll(".card")
-    let cardpego= null ;
+    const colunas = document.querySelectorAll('.cards-coluna');
 
-    const dragStart = (event) =>
-    {
-        cardpego = event.target;
-       event.dataTransfer.effectAllowed = "move";
-    };
+let cardpego = null;
 
-    const dragOver = (event)=>{
-        event.preventDefault();
-    };
+const dragStart = (event)=>{
+    cardpego = event.target;
+    event.dataTransfer.effectAllowed = "move";
 
-    const dragEnter = ({target}) => {
-        if (target.classList.contains("card-colunas")) {
-            target.classList.add("foco-coluna")
-        }
+};
+
+const dragOver = (event)=> {
+    event.preventDefault()
+
+}
+
+const dragEnter = ({ target }) => {
+    if (target.classList.contains("cards-coluna")) {
+        target.classList.add("foco-coluna");
     }
+};
 
-    const dragLeave = ({target}) => {
+const dragLeave = ({ target }) => {
+    target.classList.remove("foco-coluna")
+}
+
+const drop = ({target}) => {
+    
+    if (target.classList.contains("cards-coluna")) {
         target.classList.remove("foco-coluna")
+         target.append(cardpego)
     }
+}
 
-    const drop = ({target}) => {
-        if(!target.classList.contains('card-colunas')) return;
+const criarCard = ({ target }) => {
+    if (target.classList.contains("cards-coluna")){
+        const card = document.createElement("section")
+        card.className = "card"
+        card.draggable = "true"
 
-        target.append(cardpego)
-        target.classList.remove('foco-coluna')
+        card.addEventListener("dragstart", dragStart)
+        card.addEventListener("focusout",()=> {
+            card.contentEditable = "false"
+            if (!card.textContent) card.remove()
+        })
+        card.contentEditable = "true"
+        target.append(card)
+        card.focus()
+
     }
-
-    const criarCard = ({ target }) => {
-        if (target.classList.contains("card-colunas")){
-            const card = document.createElement("section")
-            card.className = "card"
-            card.draggable = "true"
     
-            card.addEventListener("dragstart", dragStart)
-            card.addEventListener("focusout",()=> {
-                card.contentEditable = "false"
-                if (!card.textContent) card.remove()
-            })
-            card.contentEditable = "true"
-            target.append(card)
-            card.focus()
-    
-        }
-        
-    }
+}
 
-     cards.forEach((card)=>{
-         card.addEventListener("dragstart", dragStart)
-     })
+colunas.forEach((coluna) => {
+    coluna.addEventListener("dragover", dragOver)
+    coluna.addEventListener("dragenter", dragEnter);
+    coluna.addEventListener("dragleave", dragLeave)
+    coluna.addEventListener("drop", drop)
+    coluna.addEventListener("dblclick", criarCard)
 
-    colunas.forEach((coluna) =>{
-        coluna.addEventListener('dragover' , dragOver);
-        coluna.addEventListener("dragenter", dragEnter);
-        coluna.addEventListener('dragleave', dragLeave);
-        coluna.addEventListener('drop', drop);
-        coluna.addEventListener("dblclick", criarCard)
-    })
+})
     
     return (
         <>
         <section className="card-colunas">
-
+        
         </section>
         </>
     )
